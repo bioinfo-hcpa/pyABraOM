26c8cd6feccef146b7bec5bd9177103427c30740
paolabc <pa0la_barcellosca@hotmail.com>
2021-03-29 17:10:32 -0300

    update to batch search

--- pyabraom.py
+++ pyabraom.py
@@ -3,17 +3,17 @@ import pandas as pd
 import time
 
 
-def genome_version(version:str):
+def Genome_version(version:str):
 
-          if version=="hg38" or "GRCh38":
+          if version in ["hg38","GRCh38"]:
               version = "abraomdbgenoma"
 
-          elif version=="hg19" or "GRCh37":
+          elif version in ["hg19", "GRCh37"]:
               version= "abraomdb"
           else:
-             raise Exception("Error:This version is not present in AbraOM")
+             raise Exception("Error: This genome version is not part of ABraOM project.Try search for hg19 or hg38 version.")
 
-          return version                                 
+          return  version                                
 
  
 def Request(version:str,query:str,GATK_PASS=False):
@@ -29,47 +29,39 @@ def Request(version:str,query:str,GATK_PASS=False):
 
         return response.json()
 
-def process_data(df,CEGH_Filter):
+def Process_data(df,CEGH_Filter,Variant_ID,gene):
+
     Chr = []
     start =[]
     ref = []
     alt =[]
-    gene = []
+    gene_list = []
     Filter =[]
-    prediction_func=[]
+    predfunc=[]
     allele_number=[]
     allele_alt_count=[]
     homozygous_alt_allele=[]
-    frequencie =[]
+    frequency =[]
     CEGH=[]
-    Variant_ID=[]
+    Variant_ID_list=[]
+    hemizygous =[]
     data= pd.DataFrame()
     for i in range(0,len(df["data"])):
+      if gene.upper() in df["data"][i]["Gene_refGene"]:
         Chr.append(df["data"][i]["Chr"])
 
         start.append(df["data"][i]["Start"])
-        if len(df["data"][i]["Ref"])<=10:
-           ref.append(df["data"][i]["Ref"])
-        else:
-           ref_object =df["data"][i]["Ref"]
-           ref.append(ref_object[:3])
 
-        if len(df["data"][i]["Alt"])<=3:
-           alt.append(df["data"][i]["Alt"])
-        else:
-           alt_object =df["data"][i]["Alt"]
-           alt.append(alt_object[:3])
+        ref.append(df["data"][i]["Ref"])
 
-        if len(df["data"][i]["Gene_refGene"]) ==1:
-           gene.append(df["data"][i]["Gene_refGene"])
+        alt.append(df["data"][i]["Alt"])
 
-        else:
-           gene = (df["data"][i]["Gene_refGene"]).split()
-           gene = gene[0]
+        gene_name = (df["data"][i]["Gene_refGene"]).split(";")
+        gene_list.append(gene_name[0])
 
         Filter.append(df["data"][i]["FILTER"])
 
-        prediction_func.append(df["data"][i]["PredictedFunc_refGene"])
+        predfunc.append(df["data"][i]["PredictedFunc_refGene"])
         
         allele_number.append(df["data"][i]["Allele_number"])
 
@@ -77,74 +69,129 @@ def process_data(df,CEGH_Filter):
 
         homozygous_alt_allele.append(df["data"][i]["HomozygousALT_count"])
 
-        frequencie.append(df["data"][i]["Frequencies"])
+        frequency.append(df["data"][i]["Frequencies"])
+
         if CEGH_Filter==True:
            CEGH.append(df["data"][i]["CEGH_Filter"])
 
-        ID= df["data"][i]["avsnp147"].split()
-        Variant_ID.append(ID[4].replace('</span><div',''))
+        if df["data"][i]["Chr"] in ['X','Y']:
+           hemizygous.append(df["data"][i]["Hemizygous_count"])
 
-    data["Chr"] = Chr
-    data["Start"]=start
-    data["Ref"]= ref
-    data["Alt"]=alt
+        ID= df["data"][i]["avsnp147"].split()
+        Variant_ID_list.append(ID[4].replace('</span><div',''))
+
+    data["Chromosome"] = Chr
+
+    data["Position"]=start
+
+    data["Reference"]= ref
+
+    data["Alternative"]=alt
+
+    data["Annotation"]=predfunc
+
+    data["Gene"]=gene_list
+
     if Variant_ID==True:
-       data["ID"]=Variant_ID  
-    data["Gene"]=gene
+       data['ID'] =  [i if i != 'NA' else '-' for i in Variant_ID_list]
+
     data["GATK Filter"]=Filter
+
     if CEGH_Filter==True:
        data["CEGH"]=CEGH
-    data["Annotation"]=prediction_func
-    data["Homozygotes"]= homozygous_alt_allele
+
+    data["Number of Homozygotes"]= homozygous_alt_allele
+
+    if len(hemizygous)!= 0:
+        data["Number of Hemizygous"]= hemizygous
+
     data["Allele Number"]= allele_number
-    data["Allele Alt"]= allele_alt_count
-    data["Frequencie"]= frequencie
+
+    data["Allele Count"]= allele_alt_count
+
+    data["Allele Frequency"]= frequency
 
     return data
 
-def dataframe_adjust(dataframe):
+def Variant_ID_biovar(dataframe):
+    list1= []
+    for i in range(0,len(dataframe)):
+      if len(dataframe['Alternative'][i])==1: 
+        variant_ID_biovar = dataframe['Chromosome'][i]+'-'+dataframe['Position'][i]+'-'+dataframe['Reference'][i]+'-'+dataframe['Alternative'][i]
+        frequency = dataframe['Allele Frequency'][i]
+        list1.append([variant_ID_biovar,frequency])
+    return list1
+
+
+def Dataframe_adjust(dataframe):
     pd.set_option('display.max_column',None)
     pd.set_option('display.max_rows',None)
     pd.set_option('display.max_seq_items',None)
-    pd.set_option('display.max_colwidth', 600)
+    pd.set_option('display.max_colwidth', 20)
     pd.set_option('expand_frame_repr', True)
-    pd.set_option('display.colheader_justify','right')
+    pd.set_option('display.colheader_justify','left')
 
     return dataframe
 
+def Searches(lista:list):
+     appended_data = []
 
-def Search_gene(version:str,gene:str,CEGH_Filter=False,GATK_PASS=False,Process_data=True):
+     for i in lista:
+         data = i
+         appended_data.append(data)
+
+     final_data = pd.concat(appended_data, ignore_index=True)
+
+     final_data['Hemizygous'] = final_data['Hemizygous'].fillna(0)
+
+     final_data['ID'] = final_data['ID'].fillna('-')
+
+     return final_data
+
+
+def Search_gene(version:str,gene:str,CEGH_Filter= False,GATK_PASS= False,Variant_ID= False, Process= True):
          
         response = Request(version,gene,GATK_PASS)
-        if Process_data==True:
-            result = process_data(response,CEGH_Filter)
-            data= dataframe_adjust(result)
+
+        if Process==True:
+            result = Process_data(response,CEGH_Filter,Variant_ID,gene)
+            data= Dataframe_adjust(result)
+
         else:
            data= response
 
         return data
        
 
-def Search_region(version:str,chromosome,start,end,CEGH_Filter=False,Process_data=True):
-        region = "%d"":""%d""-""%d" %(chromosome,start,end)
-        response = Request(version,region)
-        if Process_data==True:
-           result = process_data(response,CEGH_Filter)
-           dataframe= dataframe_adjust(result)
+def Search_region(version:str,chromosome,start,end,CEGH_Filter= False,Variant_ID= False, Process= True):
+
+        if start > end:
+            raise Exception('End needs to be greater than start position.')
+
         else:
-           data= response
+            region = "%s"":""%d""-""%d" %(chromosome,start,end)
+            response = Request(version,region)
+
+            if Process==True:
+              result = Process_data(response,CEGH_Filter,Variant_ID)
+              dataframe= Dataframe_adjust(result)
+
+            else:
+              data= response
 
         return dataframe
 
 
+def Variant_ID(version:str,variant:str,CEGH_Filter= False,GATK_PASS= False,Process= True):
 
-
-def Variant_ID(version:str,variant:str,CEGH_Filter=False,GATK_PASS=False,Process_data=True):
     response = Request(version,variant,GATK_PASS)
-    if Process_data==True:
-       result = process_data(response,CEGH_Filter)
-       data= dataframe_adjust(result)
+
+    if Process==True:
+       result = Process_data(response,CEGH_Filter)
+       data= Dataframe_adjust(result)
+
     else:
        data= response
-    
+
     return data
+    
--- setup.py
+++ setup.py
@@ -2,14 +2,19 @@ from setuptools import setup, find_packages
 
 
 setup(
-  name= "pyabraom",
-  version="1.0",
-  description= "A package to search variants in ABraOM database",
+  name= "pyABraOM",
+  version = '0.0.1',
+  description= "'A Python API to communicate with ABraOM database",
   author ="Paola Carneiro and Felipe Colombeli",
   author_email= "pa0la_barcellosca@gmail.com and fcolombelli@inf.ufrgs.br ",
   url="abraompackage.com",
   license= "MIT License",
-  install_requires=["requests","pandas","times"],
-  py_modules=["Search_gene","Search_Region","Variant_ID"],
+  install_requires=["requests>=2.23.0",
+                    "pandas>=1.1.5",
+                    "times"],
+  py_modules=["Search_gene",
+              "Search_Region",
+              "Variant_ID", 
+              'Searches'],
   packages =find_packages()
 )
