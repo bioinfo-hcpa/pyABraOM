fbfb3a11be36efb221cd7abfc974c1948470ac67
lola <paolabarcellosca@gmail.com>
2021-07-14 13:07:58 -0300

    Fixthe script

--- pyabraom.py
+++ pyabraom.py
@@ -1,10 +1,11 @@
 import requests
 import pandas as pd
-import time
 from pandas import json_normalize
+import time
+from Bio import Entrez, SeqIO
 import pkg_resources
 
-def genome_version(version:str):
+def Genome_version(version:str):
  
           if version=="hg38" or "GRCh38":
               version = "abraomdbgenoma"
@@ -21,7 +22,7 @@ def Request(version:str,query:str,GATK_PASS=False):
  
         url ="https://abraom.ib.usp.br/script.php" 
  
-        version = genome_version(version)
+        version = Genome_version(version)
         certificate_path = pkg_resources.resource_filename('pyabraom', 'CertBundle.pem')
 
         if GATK_PASS==False:
@@ -32,138 +33,254 @@ def Request(version:str,query:str,GATK_PASS=False):
  
         return response.json()
  
-def process_data(df,CEGH_Filter):
-    chr = []
+def Process_data(df,CEGH_Filter,Variant_ID):
+
+    Chr = []
     start =[]
     ref = []
     alt =[]
-    gene = []
-    filter =[]
-    prediction_func=[]
+    gene_list = []
+    Filter =[]
+    predfunc=[]
     allele_number=[]
     allele_alt_count=[]
     homozygous_alt_allele=[]
-    frequencie =[]
+    frequency =[]
     CEGH=[]
-    Variant_ID=[]
+    Variant_ID_list=[]
+    hemizygous =[]
     data= pd.DataFrame()
     for i in range(0,len(df["data"])):
-        chr.append(df["data"][i]["Chr"])
- 
+        Chr.append(df["data"][i]["Chr"])
+
         start.append(df["data"][i]["Start"])
-        if len(df["data"][i]["Ref"])<=10:
-           ref.append(df["data"][i]["Ref"])
-        else:
-           ref_object =df["data"][i]["Ref"]
-           ref.append(ref_object[:3])
- 
-        if len(df["data"][i]["Alt"])<=3:
-           alt.append(df["data"][i]["Alt"])
-        else:
-           alt_object =df["data"][i]["Alt"]
-           alt.append(alt_object[:3])
- 
-        if len(df["data"][i]["Gene_refGene"]) ==1:
-           gene.append(df["data"][i]["Gene_refGene"])
- 
-        else:
-           gene = (df["data"][i]["Gene_refGene"]).split()
-           gene = gene[0]
- 
-        filter.append(df["data"][i]["FILTER"])
- 
-        prediction_func.append(df["data"][i]["PredictedFunc_refGene"])
+
+        ref.append(df["data"][i]["Ref"])
+
+        alt.append(df["data"][i]["Alt"])
+
+        gene_name = (df["data"][i]["Gene_refGene"]).split(";")
+        gene_list.append(gene_name[0])
+
+        Filter.append(df["data"][i]["FILTER"])
+
+        predfunc.append(df["data"][i]["PredictedFunc_refGene"])
         
         allele_number.append(df["data"][i]["Allele_number"])
- 
+
         allele_alt_count.append(df["data"][i]["Allele_ALT_count"])
- 
+
         homozygous_alt_allele.append(df["data"][i]["HomozygousALT_count"])
- 
-        frequencie.append(df["data"][i]["Frequencies"])
+
+        frequency.append(df["data"][i]["Frequencies"])
+
         if CEGH_Filter==True:
            CEGH.append(df["data"][i]["CEGH_Filter"])
- 
+
+        if df["data"][i]["Chr"] in ['X','Y']:
+           hemizygous.append(df["data"][i]["Hemizygous_count"])
+
         ID= df["data"][i]["avsnp147"].split()
-        Variant_ID.append(ID[4].replace('</span><div',''))
- 
-    data["Chr"] = chr
-    data["Start"]=start
-    data["Ref"]= ref
-    data["Alt"]=alt
+        Variant_ID_list.append(ID[4].replace('</span><div',''))
+
+    data["Chromosome"] = Chr
+
+    data["Position"]=start
+
+    data["Reference"]= ref
+
+    data["Alternative"]=alt
+
+    data["Annotation"]=predfunc
+
+    data["Gene"]=gene_list
+
     if Variant_ID==True:
-       data["ID"]=Variant_ID  
-    data["Gene"]=gene
-    data["GATK Filter"]=filter
+       data['rsID'] =  [i if i != 'NA' else '-' for i in Variant_ID_list]
+
+    data["GATK Filter"]=Filter
+
     if CEGH_Filter==True:
-       data["CEGH"]=CEGH
-    data["Annotation"]=prediction_func
-    data["Homozygotes"]= homozygous_alt_allele
+       data["CEGH Filter"]=CEGH
+
+    data["Number of Homozygotes"]= homozygous_alt_allele
+
+    if len(hemizygous)!= 0:
+        data["Number of Hemizygotes"]= hemizygous
+
     data["Allele Number"]= allele_number
-    data["Allele Alt"]= allele_alt_count
-    data["Frequencie"]= frequencie
- 
+
+    data["Allele Count"]= allele_alt_count
+
+    data["Allele Frequency"]= frequency
+
     return data
-'''
-def style(dataframe):
-    dataframe = dataframe.style.set_table_styles(
-    [{'selector':'th', 
-      'props':[('font_family',' Sans-serif'),('text-align', 'center'),('background',' #FFFFFF'),("font-size", "10pt"),('font-weight','bold')]},
-     
-   {'selector':'td','props':[('font_family',' Times New Rowman'),('text-align', 'center'),("font-size", "10pt"),('width', '350px')]},
- 
-   {'selector':'tr:nth-of-type(odd)','props':[('background',' #f3f2f2')]},
- 
-   {'selector':'td:nth-child(1)','props':[('font-weight','bold')]},
- 
-   {'selector':'tr:hover','props':[('background','#bfbfbf')]}]).hide_index()
- 
-    return dataframe
-'''
-def dataframe_adjust(dataframe):
+
+
+
+def Dataframe_adjust(dataframe):
     pd.set_option('display.max_column',None)
     pd.set_option('display.max_rows',None)
     pd.set_option('display.max_seq_items',None)
-    pd.set_option('display.max_colwidth', 600)
+    pd.set_option('display.max_colwidth', 20)
     pd.set_option('expand_frame_repr', True)
-    pd.set_option('display.colheader_justify','right')
- 
- 
+    pd.set_option('display.colheader_justify','left')
+
     return dataframe
- 
- 
-def Search_gene(version:str,gene:str,CEGH_Filter=False,GATK_PASS=False,Process_data=True):
+
+def Searches(lista:list):
+     appended_data = []
+
+     for i in lista:
+         data = i
+         appended_data.append(data)
+
+     final_data = pd.concat(appended_data, ignore_index=True)
+     if 'Number of Hemizygotes' in final_data.columns:
+         final_data['Number of Hemizygotes'] = final_data['Number of Hemizygotes'].fillna(0)
+     if 'Variant_ID' in final_data.columns:
+         final_data['Variant_ID'] = final_data['Variant_ID'].fillna('-')
+
+     return final_data
+
+
+def Search_gene(version:str,gene:str,CEGH_Filter= False,GATK_PASS= False,Variant_ID=False, Process= True):
          
         response = Request(version,gene,GATK_PASS)
-        if Process_data==True:
-            result = process_data(response,CEGH_Filter)
-            data= dataframe_adjust(result)
+
+        if Process==True:
+            result = Process_data(response,CEGH_Filter,Variant_ID)
+            data= Dataframe_adjust(result)
+
         else:
            data= response
- 
+
         return data
        
- 
-def Search_region(version:str,chromosome,start,end,CEGH_Filter=False,Process_data=True):
-        region = "%d"":""%d""-""%d" %(chromosome,start,end)
-        response = Request(version,region)
-        if Process_data==True:
-           result = process_data(response,CEGH_Filter)
-           data= dataframe_adjust(result)
+
+def Search_region(version:str,chromosome,start,end,CEGH_Filter= False,Variant_ID=False, Process= True):
+
+        if start > end:
+            raise Exception('End needs to be greater than start position.')
+
         else:
-           data= response
- 
-        return data
- 
- 
- 
- 
-def Variant_ID(version:str,variant:str,CEGH_Filter=False,GATK_PASS=False,Process_data=True):
+            region = "%d"":""%d""-""%d" %(chromosome,start,end)
+            response = Request(version,region)
+
+            if Process==True:
+              result = Process_data(response,CEGH_Filter,Variant_ID)
+              dataframe= Dataframe_adjust(result)
+
+            else:
+              data= response
+
+        return dataframe
+
+
+def Variant_ID(version:str,variant:str,CEGH_Filter= False,GATK_PASS= False,Process= True):
+
     response = Request(version,variant,GATK_PASS)
-    if Process_data==True:
-       result = process_data(response,CEGH_Filter)
-       data= dataframe_adjust(result)
+    Variant_ID=False
+    if Process==True:
+       result = Process_data(response,CEGH_Filter,Variant_ID)
+       data= Dataframe_adjust(result)
+
     else:
        data= response
-    
+
     return data
+
+def genome_ref_info(chromosome,version,position):
+      chromosomes_hg38 = {1:568815597,
+                    2:568815596,
+                    3:568815595,
+                    4:568815594,
+                    5:568815593,
+                    6:568815592,
+                    7:568815591,
+                    8:568815590,
+                    9:568815589,
+                    10:568815588,
+                    11:568815587,
+                    12:568815586,
+                    13:568815585,
+                    14:568815584,
+                    15:568815583,
+                    16:568815582,
+                    17:568815581,
+                    18:568815580,
+                    19:568815579,
+                    20:568815578,
+                    21:568815577,
+                    22:568815576,
+                    'X':568815575,
+                    'Y':568815574}
+
+
+      chromosomes_hg37 = {1:224589800,
+                    2:224589811,
+                    3:224589815,
+                    4:224589816,
+                    5:224589817,
+                    6:224589818,
+                    7:224589819,
+                    8:224589820,
+                    9:224589821,
+                    10:224589801,
+                    11:224589802,
+                    12:224589803,
+                    13:224589804,
+                    14:224589805,
+                    15:224589806,
+                    16:224589807,
+                    17:224589808,
+                    18:224589809,
+                    19:224589810,
+                    20:224589812,
+                    21:224589813,
+                    22:224589814,
+                    'X':224589822,
+                    'Y':224589823}
+
+      if version in ["hg38","GRCh38"]:
+        handle = Entrez.efetch(db="nucleotide", 
+                              id= str(chromosomes_hg38[chromosome]) ,
+                              rettype="fasta", 
+                              strand=1, 
+                              seq_start=position, 
+                              seq_stop=position)
+        record = SeqIO.read(handle, "fasta")
+        handle.close()
+        return str(record.seq)
+      if version in ["hg19", "GRCh37"]:
+        handle = Entrez.efetch(db="nucleotide", 
+                              id= str(chromosomes_hg37[chromosome]) ,
+                              rettype="fasta", 
+                              strand=1, 
+                              seq_start=position, 
+                              seq_stop=position)
+        record = SeqIO.read(handle, "fasta")
+        handle.close()
+        return str(record.seq)
+
+def Variant_ID_biovar(dataframe,version):
+    list1= []
+    for i in range(0,len(dataframe)):
+      if dataframe['Alternative'][i]=='-': 
+        next_nt=genome_ref_info(int(dataframe['Chromosome'][i]),version, int(dataframe['Position'][i])+1)
+        variant_ID_biovar = dataframe['Chromosome'][i]+'-'+dataframe['Position'][i]+'-'+dataframe['Reference'][i]+'-'+next_nt
+        frequency = dataframe['Allele Frequency'][i]
+        list1.append([variant_ID_biovar,frequency])
+
+      elif dataframe['Reference'][i]=='-': 
+        prev_nt=genome_ref_info(int(dataframe['Chromosome'][i]),version, int(dataframe['Position'][i]))
+        variant_ID_biovar = dataframe['Chromosome'][i]+'-'+dataframe['Position'][i]+'-'+prev_nt+'-'+prev_nt+dataframe['Alternative'][i]
+        frequency = dataframe['Allele Frequency'][i]
+        list1.append([variant_ID_biovar,frequency])
+      elif dataframe['Reference'][i]!='-' and dataframe['Alternative'][i]!='-':
+        variant_ID_biovar = dataframe['Chromosome'][i]+'-'+dataframe['Position'][i]+'-'+dataframe['Reference'][i]+'-'+dataframe['Alternative'][i]
+        frequency = dataframe['Allele Frequency'][i]
+        list1.append([variant_ID_biovar,frequency])
+
+
+    return list1
