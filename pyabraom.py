289d1a56d2a3f64e5ac7654494065c8f0c0c51a6
colombelli <xcolombelli@gmail.com>
2021-07-16 11:38:33 -0300

    Fix Variant_ID_biovar
    Couldn't convert chromosome X to int: changed dictionary to one using
    string type as keys.
    Couldn't concate Position to variant id string: cohersed it to str.
    Refixed dataframe/data else in Search_region.

--- pyabraom.py
+++ pyabraom.py
@@ -169,12 +169,12 @@ def Search_region(version:str,chromosome,start,end,CEGH_Filter= False,Variant_ID
 
             if Process==True:
               result = Process_data(response,CEGH_Filter,Variant_ID)
-              dataframe= Dataframe_adjust(result)
+              data= Dataframe_adjust(result)
 
             else:
               data= response
 
-        return dataframe
+        return data
 
 
 def Variant_ID(version:str,variant:str,CEGH_Filter= False,GATK_PASS= False,Process= True):
@@ -191,54 +191,54 @@ def Variant_ID(version:str,variant:str,CEGH_Filter= False,GATK_PASS= False,Proce
     return data
 
 def genome_ref_info(chromosome,version,position):
-      chromosomes_hg38 = {1:568815597,
-                    2:568815596,
-                    3:568815595,
-                    4:568815594,
-                    5:568815593,
-                    6:568815592,
-                    7:568815591,
-                    8:568815590,
-                    9:568815589,
-                    10:568815588,
-                    11:568815587,
-                    12:568815586,
-                    13:568815585,
-                    14:568815584,
-                    15:568815583,
-                    16:568815582,
-                    17:568815581,
-                    18:568815580,
-                    19:568815579,
-                    20:568815578,
-                    21:568815577,
-                    22:568815576,
+      chromosomes_hg38 = {'1':568815597,
+                    '2':568815596,
+                    '3':568815595,
+                    '4':568815594,
+                    '5':568815593,
+                    '6':568815592,
+                    '7':568815591,
+                    '8':568815590,
+                    '9':568815589,
+                    '10':568815588,
+                    '11':568815587,
+                    '12':568815586,
+                    '13':568815585,
+                    '14':568815584,
+                    '15':568815583,
+                    '16':568815582,
+                    '17':568815581,
+                    '18':568815580,
+                    '19':568815579,
+                    '20':568815578,
+                    '21':568815577,
+                    '22':568815576,
                     'X':568815575,
                     'Y':568815574}
 
 
-      chromosomes_hg37 = {1:224589800,
-                    2:224589811,
-                    3:224589815,
-                    4:224589816,
-                    5:224589817,
-                    6:224589818,
-                    7:224589819,
-                    8:224589820,
-                    9:224589821,
-                    10:224589801,
-                    11:224589802,
-                    12:224589803,
-                    13:224589804,
-                    14:224589805,
-                    15:224589806,
-                    16:224589807,
-                    17:224589808,
-                    18:224589809,
-                    19:224589810,
-                    20:224589812,
-                    21:224589813,
-                    22:224589814,
+      chromosomes_hg37 = {'1':224589800,
+                    '2':224589811,
+                    '3':224589815,
+                    '4':224589816,
+                    '5':224589817,
+                    '6':224589818,
+                    '7':224589819,
+                    '8':224589820,
+                    '9':224589821,
+                    '10':224589801,
+                    '11':224589802,
+                    '12':224589803,
+                    '13':224589804,
+                    '14':224589805,
+                    '15':224589806,
+                    '16':224589807,
+                    '17':224589808,
+                    '18':224589809,
+                    '19':224589810,
+                    '20':224589812,
+                    '21':224589813,
+                    '22':224589814,
                     'X':224589822,
                     'Y':224589823}
 
@@ -267,18 +267,18 @@ def Variant_ID_biovar(dataframe,version):
     list1= []
     for i in range(0,len(dataframe)):
       if dataframe['Alternative'][i]=='-': 
-        next_nt=genome_ref_info(int(dataframe['Chromosome'][i]),version, int(dataframe['Position'][i])+1)
-        variant_ID_biovar = dataframe['Chromosome'][i]+'-'+dataframe['Position'][i]+'-'+dataframe['Reference'][i]+'-'+next_nt
+        next_nt=genome_ref_info(dataframe['Chromosome'][i], version, int(dataframe['Position'][i])+1)
+        variant_ID_biovar = dataframe['Chromosome'][i]+'-'+str(dataframe['Position'][i])+'-'+dataframe['Reference'][i]+'-'+next_nt
         frequency = dataframe['Allele Frequency'][i]
         list1.append([variant_ID_biovar,frequency])
 
       elif dataframe['Reference'][i]=='-': 
-        prev_nt=genome_ref_info(int(dataframe['Chromosome'][i]),version, int(dataframe['Position'][i]))
-        variant_ID_biovar = dataframe['Chromosome'][i]+'-'+dataframe['Position'][i]+'-'+prev_nt+'-'+prev_nt+dataframe['Alternative'][i]
+        prev_nt=genome_ref_info(dataframe['Chromosome'][i], version, int(dataframe['Position'][i]))
+        variant_ID_biovar = dataframe['Chromosome'][i]+'-'+str(dataframe['Position'][i])+'-'+prev_nt+'-'+prev_nt+dataframe['Alternative'][i]
         frequency = dataframe['Allele Frequency'][i]
         list1.append([variant_ID_biovar,frequency])
       elif dataframe['Reference'][i]!='-' and dataframe['Alternative'][i]!='-':
-        variant_ID_biovar = dataframe['Chromosome'][i]+'-'+dataframe['Position'][i]+'-'+dataframe['Reference'][i]+'-'+dataframe['Alternative'][i]
+        variant_ID_biovar = dataframe['Chromosome'][i]+'-'+str(dataframe['Position'][i])+'-'+dataframe['Reference'][i]+'-'+dataframe['Alternative'][i]
         frequency = dataframe['Allele Frequency'][i]
         list1.append([variant_ID_biovar,frequency])
 
