2a76b8e81e039e6850df3b18d001b6b495b9f995
lola <paolabarcellosca@gmail.com>
2021-07-29 11:10:35 -0300

    Variant ID BIOVARS improvement

--- pyabraom.py
+++ pyabraom.py
@@ -2,7 +2,6 @@ import requests
 import pandas as pd
 from pandas import json_normalize
 import time
-from Bio import Entrez, SeqIO
 import pkg_resources
 
 def Genome_version(version:str):
@@ -190,97 +189,44 @@ def Variant_ID(version:str,variant:str,CEGH_Filter= False,GATK_PASS= False,Proce
 
     return data
 
+
 def genome_ref_info(chromosome,version,position):
-      chromosomes_hg38 = {'1':568815597,
-                    '2':568815596,
-                    '3':568815595,
-                    '4':568815594,
-                    '5':568815593,
-                    '6':568815592,
-                    '7':568815591,
-                    '8':568815590,
-                    '9':568815589,
-                    '10':568815588,
-                    '11':568815587,
-                    '12':568815586,
-                    '13':568815585,
-                    '14':568815584,
-                    '15':568815583,
-                    '16':568815582,
-                    '17':568815581,
-                    '18':568815580,
-                    '19':568815579,
-                    '20':568815578,
-                    '21':568815577,
-                    '22':568815576,
-                    'X':568815575,
-                    'Y':568815574}
+       
+    if version in ["hg38","GRCh38"]:
+       server = "https://rest.ensembl.org/sequence/region/human/%s:%d..%d:1?" %(chromosome,position,position)
+    if version in ["hg19", "GRCh37"]:
+       server=  "https://grch37.rest.ensembl.org/sequence/region/human/%s:%d..%d:1?" %(chromosome,position,position)
+       
+    request = requests.get(server, headers={ "Content-Type" : "text/plain"})
+    if not request.ok:
+       request.raise_for_status()
+       sys.exit()
+    return(request.text)
 
 
-      chromosomes_hg37 = {'1':224589800,
-                    '2':224589811,
-                    '3':224589815,
-                    '4':224589816,
-                    '5':224589817,
-                    '6':224589818,
-                    '7':224589819,
-                    '8':224589820,
-                    '9':224589821,
-                    '10':224589801,
-                    '11':224589802,
-                    '12':224589803,
-                    '13':224589804,
-                    '14':224589805,
-                    '15':224589806,
-                    '16':224589807,
-                    '17':224589808,
-                    '18':224589809,
-                    '19':224589810,
-                    '20':224589812,
-                    '21':224589813,
-                    '22':224589814,
-                    'X':224589822,
-                    'Y':224589823}
+def Variant_ID_biovars(df,version):
 
-      if version in ["hg38","GRCh38"]:
-        handle = Entrez.efetch(db="nucleotide", 
-                              id= str(chromosomes_hg38[chromosome]) ,
-                              rettype="fasta", 
-                              strand=1, 
-                              seq_start=position, 
-                              seq_stop=position)
-        record = SeqIO.read(handle, "fasta")
-        handle.close()
-        return str(record.seq)
-      if version in ["hg19", "GRCh37"]:
-        handle = Entrez.efetch(db="nucleotide", 
-                              id= str(chromosomes_hg37[chromosome]) ,
-                              rettype="fasta", 
-                              strand=1, 
-                              seq_start=position, 
-                              seq_stop=position)
-        record = SeqIO.read(handle, "fasta")
-        handle.close()
-        return str(record.seq)
+    df['Alternative']=df['Alternative'].str.extract('([a-zA-Z ][a-zA-Z ]+)', expand=False)
+    df1=df.copy(deep=True)
+    df1.dropna(subset = ["Alternative"], inplace=True)
+    df1.reset_index(inplace=True)
+    df['Alternative']=df['Alternative'].str.extract('([a-zA-Z ])', expand=False)
+    df2=df.copy(deep=True)
+    df2.dropna(subset = ["Alternative"], inplace=True)
+    df2.reset_index(inplace=True)
 
-def Variant_ID_biovar(dataframe,version):
     list1= []
-    for i in range(0,len(dataframe)):
-      if dataframe['Alternative'][i]=='-': 
-        next_nt=genome_ref_info(dataframe['Chromosome'][i], version, int(dataframe['Position'][i])+1)
-        variant_ID_biovar = dataframe['Chromosome'][i]+'-'+str(dataframe['Position'][i])+'-'+dataframe['Reference'][i]+'-'+next_nt
-        frequency = dataframe['Allele Frequency'][i]
-        list1.append([variant_ID_biovar,frequency])
 
-      elif dataframe['Reference'][i]=='-': 
-        prev_nt=genome_ref_info(dataframe['Chromosome'][i], version, int(dataframe['Position'][i]))
-        variant_ID_biovar = dataframe['Chromosome'][i]+'-'+str(dataframe['Position'][i])+'-'+prev_nt+'-'+prev_nt+dataframe['Alternative'][i]
-        frequency = dataframe['Allele Frequency'][i]
-        list1.append([variant_ID_biovar,frequency])
-      elif dataframe['Reference'][i]!='-' and dataframe['Alternative'][i]!='-':
-        variant_ID_biovar = dataframe['Chromosome'][i]+'-'+str(dataframe['Position'][i])+'-'+dataframe['Reference'][i]+'-'+dataframe['Alternative'][i]
-        frequency = dataframe['Allele Frequency'][i]
-        list1.append([variant_ID_biovar,frequency])
+    for i in range(0,len(df1)): 
+       prev_nt=genome_ref_info(df1['Chromosome'][i], version, int(df1['Position'][i]))
+       variant_ID_biovar = df1['Chromosome'][i]+'-'+str(df1['Position'][i])+'-'+prev_nt+'-'+prev_nt+df1['Alternative'][i]
+       list1.append((variant_ID_biovar,df1['Allele Frequency'][i]))
+
+    list2=list(df2.agg(lambda x: f"({x['Chromosome']}-{x['Position']}-{x['Reference']}-{x['Alternative']}, {x['Allele Frequency']})", axis=1))
+
+    flist=list1+list2
+
+    flist.sort(key=lambda x:x[0])
 
 
-    return list1
+    return flist
